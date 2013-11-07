8cc4ffad3c4fd0fca30dddded6a9e391a089347e
Ryohei Fushimi <fushimi.ryohe.i@gmail.com>
2013-11-07 14:45:35 +0900

    sample 4

--- cvgl/main.cpp
+++ cvgl/main.cpp
@@ -1,97 +1,114 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/photo/photo.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <stdlib.h>
+#include <stdio.h>
 
-#define FLAG 1
 #define IN_VIDEO_FILE "sample_video_input.avi"
 #define OUT_VIDEO_FILE "sample_video_output.avi"
 
-cv::Mat inpaint_mask;
-cv::Mat img0, img, inpainted;
-cv::Point prev_pt;
+int main(int argc, char *argv[]){
 
-void on_mouse(int event, int x, int y, int flags, void *){
-    if(img.empty()){
-        return;
+    int INIT_TIME = 50;
+    int width, height;
+    double B_PARAM = 1.0 / 50.0;
+    double T_PARAM = 1.0 / 200.0;
+    double Zeta = 5.0;
+    cv::VideoCapture cap;
+    cv::Mat frame;
+    cv::Mat avg_img, sgm_img;
+    cv::Mat lower_img, upper_img, tmp_img;
+    cv::Mat dst_img, msk_img;
+
+    if(argc >= 2){
+        cap.open(argv[1]);
+    }else{
+        cap.open(0);
     }
 
-    if(event == CV_EVENT_LBUTTONDOWN){
-        printf("down\n");
-        prev_pt = cv::Point(x, y);
-    }else if(event == CV_EVENT_LBUTTONUP && (flags & CV_EVENT_FLAG_LBUTTON)){
-        printf("up\n");
-        cv::Point pt(x, y);
-        img = img0.clone();
+    cap >> frame;
 
-        cv::Rect roi(fmin(pt.x, prev_pt.x), fmin(pt.y, prev_pt.y), abs(prev_pt.x - pt.x), abs(prev_pt.y - pt.y));
-        cv::Mat image_roi = img(roi);
-        cv::bitwise_not(image_roi, image_roi);
+    cv::Size s = frame.size();
 
-//        cv::rectangle(img, pt, prev_pt, cv::Scalar::all(255));
-        img0 = img.clone();
-        cv::imshow("image", img);
-        prev_pt = cv::Point(-1, -1);
-    }else if(event == CV_EVENT_MOUSEMOVE && (flags & CV_EVENT_FLAG_LBUTTON)){
-        printf("move\n");
-        cv::Point pt(x, y);
-        img = img0.clone();
+    avg_img.create(s, CV_32FC3);
+    sgm_img.create(s, CV_32FC3);
+    lower_img.create(s, CV_32FC3);
+    upper_img.create(s, CV_32FC3);
+    tmp_img.create(s, CV_32FC3);
 
-        cv::rectangle(img, pt, prev_pt, cv::Scalar::all(255));
-        cv::imshow("image", img);
-        //        prev_pt = cv::Point(-1, -1);
+    dst_img.create(s, CV_8UC3);
+    msk_img.create(s, CV_8UC1);
+
+    printf("Background statistics initialization start");
+
+    avg_img = cv::Scalar(0, 0, 0);
+
+    for(int i = 0; i < INIT_TIME; i ++){
+        cap >> frame;
+        cv::Mat tmp;
+        frame.convertTo(tmp, avg_img.type());
+        cv::accumulate(tmp, avg_img);
     }
-}
+    avg_img.convertTo(avg_img, -1, 1.0/INIT_TIME);
 
-int main(int argc, char* argv[]){
-    char *filename = (argc >= 2) ? argv[1] : (char*) "/Users/ryohei/gitrepos/cvgl/sushi.jpg";
+    avg_img = cv::Scalar(0, 0,0);
 
-    img0 = cv::imread(filename);
-    if(img0.empty()){
-        return 0;
+    for(int i = 0; i < INIT_TIME; i++){
+        cap >> frame;
+        frame.convertTo(tmp_img, avg_img.type());
+        cv::subtract(tmp_img, avg_img, tmp_img);
+        cv::pow(tmp_img, 2.0, tmp_img);
+        tmp_img.convertTo(tmp_img, -1, 2.0);
+        cv::sqrt(tmp_img, tmp_img);
+        cv::accumulate(tmp_img, sgm_img);
     }
 
-    printf("Hot keys: \n"
-           "\t ESC - quit the program\n"
-           "\t i or ENTER - run inpainting algorithm\n"
-           "\t (before running it, paint something on  the image)");
+    sgm_img.convertTo(sgm_img, -1, 1.0/INIT_TIME);
 
-    cv::namedWindow("image", 1);
+    printf("Background statistics initialization finish");
 
-    img = img0.clone();
-    inpainted = img0.clone();
-    inpaint_mask.create(img0.size(), CV_8UC1);
+    cv::namedWindow("Input", 1);
+    cv::namedWindow("FG", 1);
+    cv::namedWindow("Mask", 1);
+    cv::namedWindow("Avg", 1);
 
-    inpaint_mask = cv::Scalar(0);
-    inpainted = cv::Scalar(0);
-    imshow("image", img);
+    cv::imshow("Avg", avg_img);
 
-    cv::setMouseCallback("image", on_mouse, 0);
 
     bool loop_flag = true;
-
     while(loop_flag){
-        int c = cv::waitKey();
-        switch(c){
-            case 27: // ESC
-            case 'q':
-                loop_flag = false;
-                break;
-            case 'r':
-                inpaint_mask = cv::Scalar(0);
-                img0.copyTo(img);
-                cv::imshow("image", img);
-                break;
-            case 'i':
-            case 10:
-                cv::namedWindow("inpainted image", 1);
-                cv::inpaint(img, inpaint_mask, inpainted, 3.0, cv::INPAINT_TELEA);
-                cv::imshow("inpainted image", inpainted);
-                break;
+        cap >> frame;
+        frame.convertTo(tmp_img, tmp_img.type());
+        cv::subtract(avg_img, sgm_img, lower_img);
+        cv::subtract(lower_img, Zeta, lower_img);
+
+        cv::add(avg_img, sgm_img, upper_img);
+        cv::add(upper_img, Zeta, upper_img);
+
+        cv::inRange(tmp_img, lower_img, upper_img, msk_img);
+
+        cv::subtract(tmp_img, avg_img, tmp_img);
+        cv::pow(tmp_img, 2.0, tmp_img);
+        tmp_img.convertTo(tmp_img, -1, 2.0);
+        cv::pow(tmp_img, 0.5, tmp_img);
+
+        cv::accumulateWeighted(frame, avg_img, B_PARAM, msk_img);
+        cv::accumulateWeighted(tmp_img, sgm_img, B_PARAM, msk_img);
+
+        cv::bitwise_not(msk_img, msk_img);
+        cv::accumulateWeighted(tmp_img, sgm_img, T_PARAM, msk_img);
+
+        dst_img = cv::Scalar(0);
+        frame.copyTo(dst_img, msk_img);
+
+        cv::imshow("Input", frame);
+        cv::imshow("FG", dst_img);
+        cv::imshow("mask", msk_img);
+
+        char key = cv::waitKey(10);
+        if(key == 27){
+            loop_flag = false;
         }
     }
     return 0;
-
-
 }
