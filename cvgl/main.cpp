eefceeca3ae8ed786acccbfb0ce17307634206f1
Ryohei Fushimi <fushimi.ryohe.i@gmail.com>
2013-11-07 14:20:47 +0900

    task 1.2-1,2

--- cvgl/main.cpp
+++ cvgl/main.cpp
@@ -2,6 +2,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/photo/photo.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <stdlib.h>
 
 #define FLAG 1
 #define IN_VIDEO_FILE "sample_video_input.avi"
@@ -16,26 +17,35 @@ void on_mouse(int event, int x, int y, int flags, void *){
         return;
     }
 
-    if(event == CV_EVENT_LBUTTONUP || (!flags && CV_EVENT_FLAG_LBUTTON)){
-        prev_pt = cv::Point(-1, -1);
-    }else if(event == CV_EVENT_LBUTTONDOWN){
+    if(event == CV_EVENT_LBUTTONDOWN){
+        printf("down\n");
         prev_pt = cv::Point(x, y);
-    }else if(event == CV_EVENT_MOUSEMOVE){
+    }else if(event == CV_EVENT_LBUTTONUP && (flags & CV_EVENT_FLAG_LBUTTON)){
+        printf("up\n");
         cv::Point pt(x, y);
-        if(prev_pt.x < 0){
-            prev_pt = pt;
-        }
-        // draw a line from the start point to the current;
-        cv::line(inpaint_mask, prev_pt, pt ,cv::Scalar(255), 5, 8 ,0);
-        cv::line(img, prev_pt, pt, cv::Scalar::all(255), 5, 8 ,0);
+        img = img0.clone();
 
-        prev_pt = pt;
+        cv::Rect roi(fmin(pt.x, prev_pt.x), fmin(pt.y, prev_pt.y), abs(prev_pt.x - pt.x), abs(prev_pt.y - pt.y));
+        cv::Mat image_roi = img(roi);
+        cv::bitwise_not(image_roi, image_roi);
+
+//        cv::rectangle(img, pt, prev_pt, cv::Scalar::all(255));
+        img0 = img.clone();
         cv::imshow("image", img);
+        prev_pt = cv::Point(-1, -1);
+    }else if(event == CV_EVENT_MOUSEMOVE && (flags & CV_EVENT_FLAG_LBUTTON)){
+        printf("move\n");
+        cv::Point pt(x, y);
+        img = img0.clone();
+
+        cv::rectangle(img, pt, prev_pt, cv::Scalar::all(255));
+        cv::imshow("image", img);
+        //        prev_pt = cv::Point(-1, -1);
     }
 }
 
 int main(int argc, char* argv[]){
-    char *filename = (argc >= 2) ? argv[1] : (char*) "/Users/ryohei/gitrepos/cvgl/yukata.jpg";
+    char *filename = (argc >= 2) ? argv[1] : (char*) "/Users/ryohei/gitrepos/cvgl/sushi.jpg";
 
     img0 = cv::imread(filename);
     if(img0.empty()){
@@ -43,11 +53,12 @@ int main(int argc, char* argv[]){
     }
 
     printf("Hot keys: \n"
-           "\t ESC - quit the program"
+           "\t ESC - quit the program\n"
            "\t i or ENTER - run inpainting algorithm\n"
            "\t (before running it, paint something on  the image)");
 
     cv::namedWindow("image", 1);
+
     img = img0.clone();
     inpainted = img0.clone();
     inpaint_mask.create(img0.size(), CV_8UC1);
