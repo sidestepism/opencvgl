6aadb843cf1a76a3710cb95c4f106226c0913577
Ryohei Fushimi <fushimi.ryohe.i@gmail.com>
2013-11-11 14:22:28 +0900

    mustachify

--- cvgl/main.cpp
+++ cvgl/main.cpp
@@ -7,6 +7,9 @@
 
 int size_of_mosaic = 0;
 
+void overlayImage(const cv::Mat &background, const cv::Mat &foreground,
+                  cv::Mat &output, cv::Point2i location);
+
 int main(int argc, char *argv[]){
     // load classifier
     std::string cascadeName = "/opt/local/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml";
@@ -17,13 +20,16 @@ int main(int argc, char *argv[]){
 
     cv::namedWindow("result", 1);
     cv::namedWindow("smallImg", 1);
+    cv::namedWindow("mustache", 1);
     cv::createTrackbar("size", "result", &size_of_mosaic, 30, 0);
     cv::VideoCapture cap;
     cv::Mat frame, mosaic;
     cap.open(0);
     cap >> frame;
     double scale = 4.0;
-    cv::Mat gray, smallImg(cv::saturate_cast<int>(frame.rows/scale), cv::saturate_cast<int>(frame.cols/scale), CV_8UC1);
+    cv::Mat mustache, gray, smallImg(cv::saturate_cast<int>(frame.rows/scale), cv::saturate_cast<int>(frame.cols/scale), CV_8UC1);
+
+    mustache = cvLoadImage("/Users/ryohei/gitrepos/cvgl/mustache.png", -1);
 
     for(;;){
         cap >> frame;
@@ -41,6 +47,7 @@ int main(int argc, char *argv[]){
 
 //        std::vector<cv::Rect>::const_iterator r = faces.begin();
 //        for(; r != faces.end(); ++r){
+
         int i;
         for(i = 0; i < faces.size(); i++){
             cv::Point center;
@@ -51,13 +58,13 @@ int main(int argc, char *argv[]){
             if(size_of_mosaic < 1) size_of_mosaic = 1;
             cv::Rect roi_rect(center.x-radius, center.y-radius, radius*2, radius * 2);
             cv::Mat mosaic = frame(roi_rect);
-            cv::Mat tmp = frame(roi_rect);
-            cv::resize(mosaic, tmp, cv::Size(radius / size_of_mosaic, radius / size_of_mosaic), 0, 0);
-            cv::resize(tmp, mosaic, cv::Size(radius * 2, radius * 2), 0, 0, CV_INTER_NN);
+            cv::Mat tmp = mosaic.clone();
+
+            cv::resize(mustache, tmp, tmp.size(), 0, 0, cv::INTER_LINEAR);
+            overlayImage(mosaic, tmp, mosaic, cv::Point2i(0, 0));
         }
 
-
-
+        cv::imshow("mustache", mustache);
         cv::imshow("smallImg", smallImg);
         cv::imshow("result", frame);
         int key = cv::waitKey(10);
@@ -66,4 +73,52 @@ int main(int argc, char *argv[]){
         }
     }
     return 0;
+}
+
+// http://jepsonsblog.blogspot.jp/2012/10/overlay-transparent-image-in-opencv.html
+void overlayImage(const cv::Mat &background, const cv::Mat &foreground,
+                  cv::Mat &output, cv::Point2i location)
+{
+    background.copyTo(output);
+
+
+    // start at the row indicated by location, or at row 0 if location.y is negative.
+    for(int y = std::max(location.y , 0); y < background.rows; ++y)
+    {
+        int fY = y - location.y; // because of the translation
+
+        // we are done of we have processed all rows of the foreground image.
+        if(fY >= foreground.rows)
+            break;
+
+        // start at the column indicated by location,
+
+        // or at column 0 if location.x is negative.
+        for(int x = std::max(location.x, 0); x < background.cols; ++x)
+        {
+            int fX = x - location.x; // because of the translation.
+
+            // we are done with this row if the column is outside of the foreground image.
+            if(fX >= foreground.cols)
+                break;
+
+            // determine the opacity of the foregrond pixel, using its fourth (alpha) channel.
+            double opacity =
+            ((double)foreground.data[fY * foreground.step + fX * foreground.channels() + 3]) / 255.;
+
+
+            // and now combine the background and foreground pixel, using the opacity,
+
+            // but only if opacity > 0.
+            for(int c = 0; opacity > 0 && c < output.channels(); ++c)
+            {
+                unsigned char foregroundPx =
+                foreground.data[fY * foreground.step + fX * foreground.channels() + c];
+                unsigned char backgroundPx =
+                background.data[y * background.step + x * background.channels() + c];
+                output.data[y*output.step + output.channels()*x + c] =
+                backgroundPx * (1.-opacity) + foregroundPx * opacity;
+            }
+        }
+    }
 }
