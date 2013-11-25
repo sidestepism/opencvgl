47a7a846f663ce8531532baa47f55b5fd88745b9
Ryohei Fushimi <fushimi.ryohe.i@gmail.com>
2013-11-25 19:23:16 +0900

    opticalflow (test)

--- cvgl/main.cpp
+++ cvgl/main.cpp
@@ -7,43 +7,165 @@
 
 using namespace cv;
 using namespace std;
-
-static void help()
-{
-    // print a welcome message, and the OpenCV version
-    cout << "\nThis is a demo of Lukas-Kanade optical flow lkdemo(),\n"
-    "Using OpenCV version " << CV_VERSION << endl;
-    cout << "\nIt uses camera by default, but you can provide a path to video as an argument.\n";
-    cout << "\nHot keys: \n"
-    "\tESC - quit the program\n"
-    "\tr - auto-initialize tracking\n"
-    "\tc - delete all the points\n"
-    "\tn - switch the \"night\" mode on/off\n"
-    "To add/remove a feature point click it\n" << endl;
-}
-
-
+int calcDistance(cv::Mat prev, cv::Mat next, int deltaX, int deltaY);
+void calcOpticalFlow(cv::Mat prev, cv::Mat next, cv::Mat flow);
+void calcPartialOpticalFlow(cv::Mat prev, cv::Mat next, cv::Mat flow, int iteration);
+//cv::Point2f calcMinimize(cv::Mat prev, cv::Mat next, int size, int* minDist);
+cv::Point2f calcMinimize(cv::Mat prev, cv::Mat next, cv::Point2f ref, int size, int* minDist, int* avgDist);
+cv::Mat m1, m2, g1, g2, flow;
 
 void drawFlow(cv::Mat prev, cv::Mat flow){
     int x, y;
     int width = prev.cols;
     int height = prev.rows;
 
-    for(x = 0; x < width; x += 10){
-        for(y = 0; y < height; y += 10){
-            cv::line(prev, cv::Point(x, y), cv::Point(flow.at<cv::Point2f>(x, y).x + x, flow.at<cv::Point2f>(x, y).y + y), cv::Scalar(0, 0, 0));
-            printf("(%d, %d) -> (%f, %f)\n", x, y, flow.at<cv::Point2f>(x, y).x, flow.at<cv::Point2f>(x, y).y);
+    for(x = 10; x < width; x += 10){
+        for(y = 10; y < height; y += 10){
+
+            int fx = -flow.at<cv::Point2f>(y, x).x + x;
+            int fy = -flow.at<cv::Point2f>(y, x).y + y;
+
+//            if(x + fx < 0) fx = 0 - x;
+//            if(x + fx > width) fx = width - x;
+//            if(y + fx < 0) fx = 0 - y;
+//            if(y + fy > height) fy = height - y;
+
+            cv::line(prev, cv::Point(x, y), cv::Point(fx, fy), cv::Scalar(0, 0, 0));
+//            printf("(%d, %d) -> (%f, %f)\n", x, y, flow.at<cv::Point2f>(x, y).x, flow.at<cv::Point2f>(x, y).y);
         }
     }
 
 }
 
+void drawFlowColorMap(cv::Mat prev, cv::Mat flow){
+    int x, y;
+    int width = prev.cols;
+    int height = prev.rows;
+
+    for(x = 0; x < width; x += 1){
+        for(y = 0; y < height; y += 1){
+
+            float dx = flow.at<cv::Point2f>(y, x).x;
+            float dy = flow.at<cv::Point2f>(y, x).y;
+
+            int sat = sqrt(dx * dx + dy * dy) * 40;
+            int hue = (sat == 0) ? 0 : (double)atan2(dx, dy) / 2.0 / M_PI * 180 + 90;
+            int bri = 255;
+            cv::rectangle(prev, cv::Point(x, y), cv::Point(x+1, y+1), cv::Scalar(hue, sat, bri));
+
+//            printf("(%d, %d, %d)", hue, sat, bri);
+
+            //            printf("(%d, %d) -> (%f, %f)\n", x, y, flow.at<cv::Point2f>(x, y).x, flow.at<cv::Point2f>(x, y).y);
+        }
+    }
+    
+}
+
+
+void calcOpticalFlow(cv::Mat prev, cv::Mat next, cv::Mat flow){
+    assert(prev.cols == next.cols);
+    calcPartialOpticalFlow(prev, next, flow, 10);
+    cv::GaussianBlur(flow, flow, Size(41, 41), 41, 41);
+//    cv::blur(flow, flow, Size(10, 10));
+}
+
+void calcPartialOpticalFlow(cv::Mat prev, cv::Mat next, cv::Mat flow, int iteration)
+{
+    assert(prev.cols == next.cols);
+    assert(prev.rows == next.rows);
+
+    if(iteration == 0) return;
+    if(prev.cols < 6 || next.cols < 6){
+        return;
+    }
+
+    int minDist = INT_MAX;
+    int avgDist = 0;
+    int width = prev.cols;
+    int height = prev.rows;
+    int size = min(20, width/5);
+
+    cv::Point2f delta = calcMinimize(prev, next, flow.at<cv::Point2f>(width/2, height/2), size, &minDist, &avgDist);
+
+    if(avgDist < minDist * 1.1){
+        // コントラスト低いので 0, 0 に
+        delta = cv::Point2f(0, 0);
+    }
+
+    for(int x = 0; x < height; x += 1){
+        for(int y = 0; y < width; y += 1){
+            flow.at<cv::Point2f>(x, y) = delta;
+        }
+    }
+
+    if(minDist > 1000 && avgDist < minDist * 2){
+        Rect r1 = Rect(0, 0,              width/2, height/2);
+        Rect r2 = Rect(0, height/2,       width/2, height - height/2);
+        Rect r3 = Rect(width/2, 0,        width - width/2, height/2);
+        Rect r4 = Rect(width/2, height/2, width - width/2, height - height/2);
+        calcPartialOpticalFlow(prev(r1), next(r1), flow(r1), iteration - 1);
+        calcPartialOpticalFlow(prev(r2), next(r2), flow(r2), iteration - 1);
+        calcPartialOpticalFlow(prev(r3), next(r3), flow(r3), iteration - 1);
+        calcPartialOpticalFlow(prev(r4), next(r4), flow(r4), iteration - 1);
+    }else{
+        printf("delta: (%f, %f), size: %d minDist: %d avgDist: %d\n", delta.x, delta.y, size,  minDist, avgDist);
+    }
+
+    cv::rectangle(prev, Point2i(0, 0), Point2i(width-1, height-1), Scalar(255, 255, 255));
+    cv::rectangle(next, Point2i(0, 0), Point2i(width-1, height-1), Scalar(255, 255, 255));
+
+    return;
+}
+
+cv::Point2f calcMinimize(cv::Mat prev, cv::Mat next, cv::Point2f ref, int size, int* minDist, int* avgDist){
+    cv::Point2f minPoint = cv::Point2i(ref.x, ref.y);
+    int sumDist = 0;
+    int count = 0;
+    for(double x = -size; x <= size; x+= 1){
+        for(double y = -size; y <= size; y+= 1){
+            if(ref.x + x < 0 || ref.y + y < 0 || ref.x + x > prev.cols || ref.y + y > prev.rows) continue;
+            int dist = calcDistance(prev, next, ref.x + x, ref.y + y);
+            if(dist == INT_MAX) continue;
+            if(dist < *minDist){
+                minPoint.x = ref.x + x;
+                minPoint.y = ref.y + y;
+                *minDist = dist;
+            }
+            sumDist += dist;
+            count ++;
+        }
+    }
+    *avgDist = count != 0 ? sumDist / count : 0;
+    if(count < 10) return ref;
+    return minPoint;
+}
+
+int calcDistance(cv::Mat prev, cv::Mat next, int deltaX, int deltaY){
+    int w = prev.cols - abs(deltaX);
+    int h = prev.rows - abs(deltaY);
+    if(w < 3 || h < 3){
+        return INT_MAX;
+    }
+    Mat imgA = prev(Rect(deltaX > 0 ? 0 : -deltaX, deltaY > 0 ? 0 : -deltaY, w, h));
+    Mat imgB = next(Rect(deltaX > 0 ? deltaX : 0 , deltaY > 0 ? deltaY : 0 , w, h));
+    Mat imgC;
+    cv::absdiff(imgA, imgB, imgC);
+
+//    cv::imshow("ImgC", imgC);
+    Scalar distxy = cv::mean(imgC);
+
+
+//    printf("(%d, %d, dist: %d)\n", deltaX, deltaY, (int)(distxy.val[0] * 1024));
+    return (int)(distxy.val[0] * 1024);
+}
+
 int main( int argc, char** argv )
 {
-    cv::Mat m1, m2, g1, g2, flow;
 
-    m1 = cv::imread("/Users/ryohei/gitrepos/cvgl/stereo/m1.jpg");
-    m2 = cv::imread("/Users/ryohei/gitrepos/cvgl/stereo/m2.jpg");
+//    m1 = cv::imread("/Users/ryohei/gitrepos/cvgl/mid/eval-data/Teddy/frame10.png");
+//    m2 = cv::imread("/Users/ryohei/gitrepos/cvgl/mid/eval-data/Teddy/frame11.png");
+    m1 = cv::imread("/Users/ryohei/gitrepos/cvgl/stereo/pa0.jpg");
+    m2 = cv::imread("/Users/ryohei/gitrepos/cvgl/stereo/pa1.jpg");
 
 
     if(m1.empty() || m2.empty()){
@@ -53,30 +175,53 @@ int main( int argc, char** argv )
     cv::cvtColor(m1, g1, CV_RGB2GRAY);
     cv::cvtColor(m2, g2, CV_RGB2GRAY);
 
-    cv::imshow("m1", m1);
-    cv::imshow("m2", m2);
+//    cv::equalizeHist(g1, g1);
+//    cv::equalizeHist(g2, g2);
+
+//    cv::Canny(g1, g1, 200, 60);
+//    cv::Canny(g2, g2, 200, 60);
+
+    cv::GaussianBlur(g1, g1, Size(3, 3), 8, 6);
+    cv::GaussianBlur(g2, g2, Size(3, 3), 8, 6);
+
+//    cv::Laplacian(g1, g1, 8, 3);
+//    cv::Laplacian(g2, g2, 8, 3);
+
+//    cv::imshow("m1", m1);
+//    cv::imshow("m2", m2);
 
     // http://opencv.jp/opencv-2svn/cpp/video_motion_analysis_and_object_tracking.html?highlight=optical#calcOpticalFlowFarneback
-    cv::calcOpticalFlowFarneback(
-                                 g1,
-                                 g2,
-                                 flow,
-                                 0.7,  // pyrScale
-                                 5,    // levels
-                                 10,   // winsize
-                                 9,    // iterations
-                                 7,    // polyN
-                                 1.5,  // polySigma
-                                 OPTFLOW_FARNEBACK_GAUSSIAN);
+    flow = cv::Mat(g1.size(), CV_32FC2);
+//    cv::calcOpticalFlowFarneback(
+//                                 g1,
+//                                 g2,
+//                                 flow,
+//                                 0.8,  // pyrScale
+//                                 20,    // levels
+//                                 100,   // winsize
+//                                 15,    // iterations
+//                                 7,    // polyN
+//                                 1.5,  // polySigma
+//                                 OPTFLOW_FARNEBACK_GAUSSIAN);
 
-//    drawFlow(g1, flow);
-    cv::imshow("Optical Flow", m1);
+    calcOpticalFlow(g1, g2, flow);
+
+    cv::Mat colorMap = m1.clone();
+
+    drawFlowColorMap(colorMap, flow);
+    cv::cvtColor(colorMap, colorMap, CV_HSV2RGB);
+    drawFlow(colorMap, flow);
+
+    cv::imshow("Optical Flow", colorMap);
+
+    cv::imshow("g1", g1);
+//    cv::imshow("g2", g2);
 
     Mat mov[61];
     Mat map(flow.size(), CV_32FC2);
 
     for(int k = 0; k < 61; ++k){
-        double q = (double)(k-30) / 10; // とりあえず
+        double q = (double)(k) / 40; // とりあえず
         for (int y = 0; y < map.rows; ++y)
         {
             for (int x = 0; x < map.cols; ++x)
@@ -85,9 +230,12 @@ int main( int argc, char** argv )
                 map.at<Point2f>(y, x) = Point2f(x + f.x * q, y + f.y * q);
             }
         }
-        cv::remap(m1, mov[k], map, cv::Mat(), CV_INTER_LINEAR);
+        cv::remap(m2, mov[k], map, cv::Mat(), CV_INTER_LINEAR);
     }
+    cv::Mat kasane;
 
+    cv::addWeighted(m1, 0.5, m2, 0.5, 1, kasane);
+    cv::imshow("kasane", kasane);
 
 
     while(1){
@@ -95,7 +243,10 @@ int main( int argc, char** argv )
             cv::imshow("Flow", mov[k]);
             cv::waitKey(50);
         }
-
+        cv::imshow("Flow", m1);
+        cv::waitKey(500);
+        cv::imshow("Flow", m2);
+        cv::waitKey(500);
     }
     return 0;
 }
